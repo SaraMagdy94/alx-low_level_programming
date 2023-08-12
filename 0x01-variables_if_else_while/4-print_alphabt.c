51b9262f24c9b9c55275f3952bc0bc966194d27d
SaraMagdy94 <engazharya@gmail.com>
2023-08-11 19:42:20 -0700

    s

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+
+
+int main(void)
+
+{
+
+	char l, e, q;
+
+
+	e = 'e';
+
+	q = 'q';
+
+	l = 'a';
+
+	while (l <= 'z')
+
+	{
+
+		if (l != e && l != q)
+
+			putchar(l);
+			
+			l++;
+	}
+
+	putchar('\n');
+
+
+	return (0);
+
+}
