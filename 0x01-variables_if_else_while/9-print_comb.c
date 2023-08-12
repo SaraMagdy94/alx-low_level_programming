95ab2dcdb42d6aeedae69c02b49f1b3a7b52324e
SaraMagdy94 <engazharya@gmail.com>
2023-08-11 20:15:45 -0700

    s

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	int x = 0;
+
+
+	while (x < 10)
+
+	{
+
+		putchar(x + '0');
+
+		x++;
+
+		if (x == 10)
+
+		putchar('\n');
+
+		else
+
+		{
+
+			putchar(',');
+
+			putchar(' ');
+
+		}
+
+	}
+
+	return (0);
+
+}
