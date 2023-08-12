d017fb6f03c7c99c4267cf64cc6c951beef5793a
SaraMagdy94 <engazharya@gmail.com>
2023-08-11 20:00:13 -0700

    s

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+
+
+int main(void)
+
+{
+
+	char R;
+
+
+
+	for (R = 'z'; R >= 'a'; R--)
+
+		putchar(R);
+
+
+		putchar('\n');
+
+
+
+	return (0);
+
+}
