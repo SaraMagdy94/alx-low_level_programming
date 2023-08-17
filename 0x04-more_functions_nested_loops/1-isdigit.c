bff0762f6363375d77e9369a3a3c5793cb53c4d5
SaraMagdy94 <engazharya@gmail.com>
2023-08-17 05:19:00 -0700

    s

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - check for digits
+ * @c : integer value input
+ * Return: Always(0).
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,5 +6,6 @@
 /* Function prototype for _putchar */
 int _putchar(char c);
 int _isupper(int c);
+int _isdigit(int c);
 
 #endif /* MAIN_H */
