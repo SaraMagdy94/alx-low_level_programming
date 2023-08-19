030274c2878413def0be72eddff46731b3f6563d
SaraMagdy94 <engazharya@gmail.com>
2023-08-19 02:26:58 -0700

    s

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,21 +13,22 @@ int main(void)
 	{
 		if (r % 3 == 0 && r % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (r % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (r % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 		{
 			printf("%d ", r);
 		}
-
+		if (r != 100)
+			printf(" ");
 	}
 
 	printf("\n");
