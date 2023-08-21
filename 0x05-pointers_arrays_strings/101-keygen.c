22bb386ef3c7d662268281755fcb2c607e26a941
SaraMagdy94 <engazharya@gmail.com>
2023-08-22 00:06:58 +0300

    s

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 84
+
+int main(void)
+{
+srand(time(NULL));  
+
+char password[PASSWORD_LENGTH + 1];  
+for (int i = 0; i < PASSWORD_LENGTH; i++)
+{
+password[i] = rand() % (126 - 33 + 1) + 33;
+}
+password[PASSWORD_LENGTH] = '\0';  
+
+printf("%s\n", password);  
+
+return 0;
+}
