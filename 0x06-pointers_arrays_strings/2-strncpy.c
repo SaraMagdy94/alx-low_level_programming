371aa1bb30ab1209b6bf0ca2fbecc004ce5995f9
SaraMagdy94 <engazharya@gmail.com>
2023-08-25 01:37:01 +0300

    s

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,23 +10,29 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
+
 char *r = dest;
 
 while (n > 0 && *src != '\0')
 {
+
 *dest = *src;
 dest++;
 src++;
 n--;
+
 }
 
 while (n > 0)
 {
+
 *dest = '\0';
 dest++;
 n--;
+
 }
 
-return(r);
+return (r);
+
 }
 
