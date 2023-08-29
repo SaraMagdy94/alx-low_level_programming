9572ba63e54c15469be44f75a1766e766b7d4849
SaraMagdy94 <engazharya@gmail.com>
2023-08-29 03:42:36 +0300

    s

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+/* Function prototype for _putchar */
+int _putchar(char c);
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+
+#endif /* MAIN_H */
