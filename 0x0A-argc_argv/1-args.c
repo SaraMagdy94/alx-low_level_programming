908f19ea87e4614255cd78c0ffd1954dabd7ae99
SaraMagdy94 <engazharya@gmail.com>
2023-09-05 03:10:56 +0300

    s

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints its name,
+ * followed by a new line
+ * @argc: arguement count
+ * @argv: argument value
+ * Return: Always (0)
+ */
+
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+
+(void)argc;
+
+printf("%d\n", argc - 1);
+
+return (0);
+
+}
+
