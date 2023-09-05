98a3afef2fcf879ebda8281a01c630ba8afbbbb1
SaraMagdy94 <engazharya@gmail.com>
2023-09-05 03:34:50 +0300

    s

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - a program that prints its name,
+ * followed by a new line
+ * @argc: arguement count
+ * @argv: argument value
+ * Return: Always (0)
+ */
+
+int main(int argc, char *argv[])
+{
+
+int sum;
+int r;
+
+sum = 0;
+
+if (argc > 1)
+{
+
+for (r = 1; r < argc; r++)
+{
+
+int b = 0;
+
+while (argv[r][b] != '\0')
+{
+
+if (!isdigit(argv[r][b]))
+{
+
+printf("Error\n");
+
+return (1);
+
+}
+
+b++;
+
+}
+
+sum += atoi(argv[r]);
+
+}
+
+}
+
+else
+
+{
+
+printf("0\n");
+
+}
+
+printf("%d\n", sum);
+
+return (0);
+
+}
+
