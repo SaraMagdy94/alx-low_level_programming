2b3af5f2359b84689291b65baf82ad73ace9eb58
SaraMagdy94 <engazharya@gmail.com>
2023-09-14 00:47:35 +0300

    s

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -8,10 +8,9 @@
  * Return: Nothing
  */
 
-void print_name(char *name, void (*f)(char *name))
+void print_name(char *name, void (*f)(char *))
 {
-if (name == NULL || f == NULL)
-return;
+if (name != 0 && *name != '\0' && f != 0)
 f(name);
 }
 
