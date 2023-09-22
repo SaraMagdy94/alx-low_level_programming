647cad9df5797b9ab2da1a83820b8f150c9b6332
SaraMagdy94 <engazharya@gmail.com>
2023-09-22 16:24:36 -0700

    s

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,6 +7,7 @@ void my_startup_function(void) __attribute__ ((constructor));
 
 void my_startup_function(void)
 {
-	    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,
+\nI bore my house upon my back!\n");
 }
 
