f086a4e7ba7a10b919cfd429d16b00fc204916b1
SaraMagdy94 <engazharya@gmail.com>
2023-09-28 16:12:46 -0700

    ss

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,12 +13,12 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int r = 0;
 
-	if (b == NULL) 
+	if (b == NULL)
 		return (0);
 
 	while (*b)
 	{
-		if (*b != '0' && *b != '1') 
+		if (*b != '0' && *b != '1')
 			return (0);
 
 		r = (r << 1) + (*b - '0');
