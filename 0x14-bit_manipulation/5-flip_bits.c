69b437aa56959f10150a143a955c82c6d62f9ff3
SaraMagdy94 <engazharya@gmail.com>
2023-09-28 16:58:01 -0700

    s

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,11 +1,11 @@
-/**
-* flp_bits - Returns the number of bits needed to flip from one number to another.
-* @n: First number.
-* * @m: Second number.
-* *
-* * Return: Number of bits to flip.
-*/
 
+/**
+ * flip_bits - Returns the number of bits needed to fli
+ * from one number to another.
+ * @n: First number.
+ * @m: Second number.
+ * Return: Number of bits to flip.
+ */
 unsigned int flip_bits(unsigned int n, unsigned int m)
 {
 	unsigned int d = n ^ m;
@@ -17,6 +17,6 @@ unsigned int flip_bits(unsigned int n, unsigned int m)
 		d >>= 1;
 	}
 
-	return c;
+	return (c);
 }
 
